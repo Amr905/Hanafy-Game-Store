5ba87a3ddbeb14a6fba5adb4514370bae5ca2e20
bido8597 <bido8597@gmail.com>
2018-03-30 15:52:04 +0200

    Adding validation

---
+++ user/js/script.js
@@ -0,0 +1,10 @@
+function isEmailAddress() {
+    var x = document.forms["profile"]["email"].value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
+        alert("Not a valid e-mail");
+        return false;
+    }
+
+}
