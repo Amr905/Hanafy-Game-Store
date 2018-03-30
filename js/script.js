7c2394a15df62ae7013036b8bbd1cd0d25bad3cc
amr905 <amr905@gmail.com>
2018-03-30 18:37:29 +0200

    checkout page

--- js/script.js
+++ js/script.js
@@ -1,34 +1,41 @@
 var slideIndex = 1;
-var kimo = setInterval(showDivs, 5000);
-    function plusDivs(n) {
-        showDivs(slideIndex += n);
+var slideDelay = setInterval(showDivs, 5000);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    clearInterval(slideDelay);
+    showDivs(slideIndex = n);
+    slideDelay = setInterval(showDivs, 5000);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+
+    if (n > x.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
 
-    function currentDiv(n) {
-        clearInterval(kimo);
-        showDivs(slideIndex = n);
-        kimo = setInterval(showDivs, 5000);
+    if (slideIndex > x.length) {
+        slideIndex = 1
     }
-
-    function showDivs(n) {
-        var i;
-        var x = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-
-        if (n > x.length) { slideIndex = 1 }
-        if (n < 1) { slideIndex = x.length }
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
-
-        if (slideIndex > x.length) { slideIndex = 1 }
-        x[slideIndex - 1].style.display = "grid";
-        dots[slideIndex - 1].className += " active";
-        slideIndex++;
-    }
-    window.onload = function () {
-        showDivs(slideIndex);
-   };
+    x[slideIndex - 1].style.display = "grid";
+    dots[slideIndex - 1].className += " active";
+    slideIndex++;
+}
+window.onload = function () {
+    showDivs(slideIndex);
+};
