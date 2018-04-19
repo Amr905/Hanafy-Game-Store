c1ff6655a3885b49a45be529a59f31bed8c25fcf
amr905 <amr905@gmail.com>
2018-04-19 19:36:03 +0200

    database

--- hgs.sql
+++ hgs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 18, 2018 at 09:07 PM
+-- Generation Time: Apr 19, 2018 at 05:35 PM
 -- Server version: 5.7.21
 -- PHP Version: 5.6.35
 
@@ -36,83 +36,30 @@ CREATE TABLE IF NOT EXISTS `cart` (
   PRIMARY KEY (`id`),
   KEY `cart_product` (`product_id`),
   KEY `cart_user` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
--- Table structure for table `order`
+-- Dumping data for table `cart`
 --
 
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE IF NOT EXISTS `order` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) NOT NULL,
-  `product_id` int(10) NOT NULL,
-  `price` int(6) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_order` (`user_id`),
-  KEY `product_order` (`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+INSERT INTO `cart` (`id`, `user_id`, `product_id`) VALUES
+(1, 1, 1);
 
 --
--- Dumping data for table `order`
+-- Triggers `cart`
 --
-
-INSERT INTO `order` (`id`, `user_id`, `product_id`, `price`) VALUES
-(1, 1, 1, 123),
-(2, 2, 1, 12);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `product`
---
-
-DROP TABLE IF EXISTS `product`;
-CREATE TABLE IF NOT EXISTS `product` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(60) NOT NULL,
-  `price` int(6) NOT NULL,
-  `serial` varchar(30) NOT NULL,
-  `image` blob,
-  `url_img` text,
-  `out_of_stock` tinyint(1) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `product`
---
-
-INSERT INTO `product` (`id`, `name`, `price`, `serial`, `image`, `url_img`, `out_of_stock`) VALUES
-(1, 'test1', 123, 'aadasd', NULL, NULL, 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `fname` varchar(12) NOT NULL,
-  `lname` varchar(12) NOT NULL,
-  `mail` varchar(70) NOT NULL,
-  `password` varchar(60) NOT NULL,
-  `level` int(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `fname`, `lname`, `mail`, `password`, `level`) VALUES
-(1, 'amr', 'morsy', 'amadf', '1111', 1),
-(2, 'ds', 'sdf', 'msgsdfg', 'sdfsdf', 1),
-(3, 'fd', 'fd', 'ddffd', 'dfddf', 2);
+DROP TRIGGER IF EXISTS `delete_logger`;
+DELIMITER $$
+CREATE TRIGGER `delete_logger` BEFORE DELETE ON `cart` FOR EACH ROW INSERT INTO cart_log
+VALUES (id,old.id,old.user_id,old.product_id,"removed")
+$$
+DELIMITER ;
+DROP TRIGGER IF EXISTS `insert_logger`;
+DELIMITER $$
+CREATE TRIGGER `insert_logger` AFTER INSERT ON `cart` FOR EACH ROW INSERT INTO cart_log
+VALUES (id,new.id,new.user_id,new.product_id)
+$$
+DELIMITER ;
 
 --
 -- Constraints for dumped tables
@@ -124,13 +71,6 @@ INSERT INTO `user` (`id`, `fname`, `lname`, `mail`, `password`, `level`) VALUES
 ALTER TABLE `cart`
   ADD CONSTRAINT `cart_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `cart_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Constraints for table `order`
---
-ALTER TABLE `order`
-  ADD CONSTRAINT `product_order` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `user_order` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
